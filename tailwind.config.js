73b9513707449cf26925f9549cddf2483b1589f9
ayankz <ryskozhayev.ayan@alser.kz>
2025-03-31 02:55:23 +0500

    changes styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        header: "#588B8B",
+      },
+    },
   },
   plugins: [],
 };
